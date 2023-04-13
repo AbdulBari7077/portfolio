1761b32302d1d5c9e36d2ee041e6b30f005f664b
AbdulBari7077 <a.bari.ab64@gmail.com>
2023-04-13 11:51:47 +0500

    about subjects added

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight  ,ImMinus} from "react-icons/im";
+import { ImPointRight, ImMinus } from "react-icons/im";
 
 
 function AboutCard() {
@@ -27,6 +27,10 @@ function AboutCard() {
               <ImPointRight /> Travelling
             </li>
           </ul>
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Abdul Bari</footer>
           <p style={{ textAlign: "justify" }}>
             <br />
             <br />
@@ -34,21 +38,59 @@ function AboutCard() {
             <br />
           </p>
           <ul>
-            <li className="about-activity">
-              <ImMinus /> FAST National University, Karachi Bachelors in Computer Science , August 2019 - Present
-            </li>
-            <li className="about-activity">
-              <ImMinus /> Malir Cantt Govt Degree College HSC in Engineering June 2017 - May 2019
-            </li>
-            <li className="about-activity">
-              <ImMinus /> The Montessori Home SSC in Computer Science June 2015 - May 2017
-            </li>
+            <ul className="about-activity" style={{ fontSize: '13px' }}>
+
+              <h4>Intermediate</h4>
+              <ul>English</ul>
+              <ul>Chemistry</ul>
+              <ul>Mathematics</ul>
+              <ul>Physics</ul>
+              <ul>Urdu</ul>
+              <ul>Islamiat</ul>
+              <ul>Pakistan Studies</ul>
+              <h4>Semester 1</h4>
+              <ul>Programming Fundamentals</ul>
+              <ul>Applied Physics</ul>
+              <ul>Calculus and Analytical Geometry</ul>
+              <ul>English Composition and Comprehension</ul>
+              <ul>Islamic and Religious Studies</ul>
+              <h4>Semester 2</h4>
+              <ul>English Comprehension and Composition</ul>
+              <ul>Digital Logic Design</ul>
+              <ul>Islamic and Religious Studies</ul>
+              <ul>Differential Equations</ul>
+              <ul>Communication and Presentation Skills</ul>
+              <h4>Semester 3</h4>
+              <ul>Pakistan Studies</ul>
+              <ul>Object Oriented Programming</ul>
+              <ul>Computer Organization and Assembly Language</ul>
+              <ul>Linear Algebra</ul>
+              <ul>Psychology</ul>
+              <h4>Semester 4</h4>
+              <ul>Data Structures</ul>
+              <ul>Theory of Automata</ul>
+              <ul>Probablity and Statistics</ul>
+              <ul>Database Systems</ul>
+              <ul>Operating Systems</ul>
+              <h4>Semester 5</h4>
+              <ul>Design and Analysis of Algorithm</ul>
+              <ul>Software Design And Analysis</ul>
+              <ul>Artificial Intelligence</ul>
+              <ul>Computer Networks</ul>
+              <ul>Parallel and distributed Computing</ul>
+              <h4>Semester 6</h4>
+              <ul>Software Engineering</ul>
+              <ul>Datascience</ul>
+              <ul>Technical and Business Writing</ul>
+              <ul>Software for Mobile Devices</ul>
+              <h4>Semester 7</h4>
+              <ul>Information Security</ul>
+              <ul>Agile Software Project Management</ul>
+            </ul>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Abdul Bari</footer>
+         
+          
         </blockquote>
       </Card.Body>
     </Card>
